ffefa2673a76c64f0628e8fd989419ada739baf7
tienquan1999 <zed.quan99@gmail.com>
2018-12-11 21:56:47 +0700

    fix

--- router/webhook.js
+++ router/webhook.js
@@ -2,50 +2,47 @@ const express = require('express');
 const router = express.Router();
 const VERIFY_TOKEN = "f34e8172d38f4001621b11f25c7dbe08";
 const APP_SECRET = "af146db21aa912f1b36bbf7d21a9d306";
-const PAGE_ACCESS_TOKEN = "EAAfBIGSlNtgBABPJ3I7Tuvi2yndxxI57LlQ95oswci7K5p1MVUWZBRzkVhkZB3Qct47t2P9263kvE88teyUWnOv5IZB4ICYcNWZChiBTNFSmWt4XsHsv0QS1CgWWutBtjRg2srTYyjmDAbbEpG4bCcHZCLxHjQ9N8Pnp1wt9OrQZDZD";
+const PAGE_ACCESS_TOKEN = "EAAFvhxfFehoBAE6W5ZBQKanfeRvDIs9XZA3DfCPTwgZBeuURfL6K92bwvEDu4rYz1osTR5ZC0lZAQu6nMvyIfzmPTvQsCHqMefoZAzz4MMZAHLv04GMDFKwi0ioD5THFQ3z2DJJ3Au6ZCnpzYMBAvCCZBXVqyCrjhj3EA3SbhoACxYQZDZD";
 
 router.get('/webhook', (req, res) => {
-
-    let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
-    console.log("mode: \n", mode);
-    console.log("token: \n", token);
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
 
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Responds with the challenge token from the request
+        if (token === VERIFY_TOKEN) {
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
-
         } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
         }
-    }
 });
 
 // Creates the endpoint for our webhook
 router.post('/webhook', (req, res) => {
-    var entries = req.body.entry;
-    for (var entry of entries) {
-        console.log(entry);
-        var messaging = entry.messaging;
-        for (var message of messaging) {
-            var senderId = message.sender.id;
-            if (message.message) {
-                if (message.message.text) {
-                    var text = message.message.text;
-                    sendMessage(senderId, "Hello!! I'm a bot. Your message: " + text);
-                }
-            }
-        }
+
+    // Parse the request body from the POST
+    let body = req.body;
+    console.log(req.body);
+
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        body.entry[0].changes.forEach(function(entry) {
+
+            // Gets the body of the webhook event
+            let webhook_event = entry.value;
+            console.log(webhook_event);
+        });
+
+        // Return a '200 OK' response to all events
+        res.status(200).send('EVENT_RECEIVED');
+
+    } else {
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
     }
-    res.status(200).send("OK");
-});
+
+    });
 
 function sendMessage(senderId, message) {
     request({
