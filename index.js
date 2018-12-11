5de458b40ef6e32a63ea4193ea10e9b72a8c3b40
tienquan1999 <zed.quan99@gmail.com>
2018-12-11 11:20:27 +0700

    oki

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const body_parser = require('body-parser');
 const morgan = require('morgan');
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(body_parser.json({ limit: '100mb' }));
@@ -8,7 +9,7 @@ app.use(body_parser.urlencoded({ limit: '100mb', extended: true }));
 app.use(morgan('default'));
 
 app.use('/api', require('./router/webhook'));
-app.listen(3000, err => {
+app.listen(port , err => {
     if(!err){
         console.log("Server is running on 3000");
     }
